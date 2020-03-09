3e8f32aa09d93983d440a3882efca2c9df6c7ae4
Jaygk <957569035@qq.com>
2020-03-09 21:24:32 +0800

    :zap: 增加loading组件,等待加载

--- src/views/search/Search.vue
+++ src/views/search/Search.vue
@@ -2,43 +2,56 @@
   <div class="search">
     <!-- 搜索框 -->
     <div class="box">
-      <search-box :searchDefault="searchDefault" />
+      <search-box @search="toSearch" :searchDefault="searchDefault" />
     </div>
-    <div class="wrapper">
+    <div class="wrapper" ref="wrapper">
       <scroll :data="HotSearch" class="scroll" ref="scroll">
         <div>
           <!-- 搜索历史 -->
           <history />
           <!-- 热门搜索 -->
-          <hot-search :hotList="HotSearch" />
+          <hot-search @select="toSearch" :hotList="HotSearch" />
         </div>
       </scroll>
+    </div>
+
+    <!-- 加载等待组件 -->
+    <div class="loading-container" v-show="!HotSearch.length">
+      <loading />
     </div>
   </div>
 </template>
 
 <script>
-import SearchBox from './childrenCpts/SearchBox'
+import SearchBox from 'components/searchBox/SearchBox'
 import History from './childrenCpts/History'
 import HotSearch from './childrenCpts/HotSearch'
 import Scroll from 'components/scroll/Scroll'
+import Loading from 'components/loading/Loading'
 
 import { getSearchDefault, getHotSearch } from 'api/search'
+import { playlistMixin } from 'utils/mixins'
 
 export default {
+  mixins: [playlistMixin],
   data() {
     return {
       searchDefault: null,
-      HotSearch: null
+      HotSearch: []
     }
   },
   components: {
     SearchBox,
     History,
     HotSearch,
-    Scroll
+    Scroll,
+    Loading
   },
   methods: {
+    handlePlaylist(playlist) {
+      const bottom = playlist.length > 0 ? '60px' : '0'
+      this.$refs.wrapper.style.bottom = bottom
+    },
     async _getSearchDefault() {
       const res = await getSearchDefault()
       this.searchDefault = res.data
@@ -48,11 +61,26 @@ export default {
     async _getHotSearch() {
       const res = await getHotSearch()
       this.HotSearch = res.data
+    },
+
+    refresh() {
+      this.$nextTick(() => {
+        this.$refs.scroll.refresh()
+      })
+    },
+    toSearch(keywords) {
+      this.$router.push({
+        name: 'result',
+        params: {
+          keywords
+        }
+      })
     }
   },
   activated() {
     this._getSearchDefault()
     this._getHotSearch()
+    this.refresh()
   }
 }
 </script>
@@ -60,8 +88,8 @@ export default {
 <style lang="stylus" scoped>
 @import "~assets/stylus/variable"
 .search
-  .box 
-    margin: 6px 12px
+  .box
+    margin: 6px 12px 0 12px
   .wrapper
     position: fixed
     top: 168px
@@ -71,4 +99,9 @@ export default {
   .scroll
     height: 100%
     overflow: hidden
+  .loading-container
+    position: absolute
+    width: 100%
+    top: 50%
+    transform: translateY(-50%)
 </style>
