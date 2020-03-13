762fcb032a870f0ba64c013dd968699520ddc4c4
Jaygk <957569035@qq.com>
2020-03-13 18:58:18 +0800

    :lipstick: 缩短歌曲列表中序号与歌名之间的距离

--- src/views/search/childrenCpts/HotSearch.vue
+++ src/views/search/childrenCpts/HotSearch.vue
@@ -76,7 +76,7 @@ export default {
     .rank
       flex: 0 0 25px
       width: 25px
-      margin-right: 30px
+      margin-right: 20px
       text-align: center
       .icon
         display: inline-block
